d41e62a5cfc82fd323b0cbc747f8367b1c0cc978
tkyzmina <tkyzmia@gmail.com>
2022-08-11 14:06:59 +0300

    (feature): Add loading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import { useState, useEffect } from "react";
-import { Typography, Container } from "@mui/material";
+import { Typography, Container, Skeleton } from "@mui/material";
 import CharactersList from "./components/CharactersList";
 const url = "https://swapi.dev/api/people";
 
 function App() {
   const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const fetchCharacters = async () => {
+    setLoading(true);
     const response = await fetch(url);
     const data = await response.json();
     setCharacters(data.results);
+    setLoading(false);
   };
-  
+
   useEffect(() => {
     fetchCharacters();
   }, []);
 
+  if (loading) {
+    return (
+      <Container>
+        <Typography variant="h4" component="h1" align="center" sx={{ mb: 4 }}>
+          Loading...
+        </Typography>
+        {[...Array(8)].map((item, index) => {
+          return (
+            <Skeleton
+              variant="rounded"
+              height={40}
+              sx={{ mb: 2, width: 1 }}
+              key={index}
+            />
+          );
+        })}
+      </Container>
+    );
+  }
   return (
     <div className="App">
       <Container>
+        {" "}
+        {}
         <Typography variant="h4" component="h1" align="center" sx={{ mb: 4 }}>
           Список людей
         </Typography>
